c794cfa8cf61cd62fa28020ebebfc02109c6b24f
Your Name <aaadntjr@naver.com>
2025-12-01 13:49:43 +0900

    Add meta tags for SEO and social sharing in _document.tsx; update favicon.ico

--- design-to-markup-main/src/pages/_document.tsx
+++ design-to-markup-main/src/pages/_document.tsx
@@ -11,6 +11,30 @@ export default function Document() {
           type='font/woff2'
           crossOrigin='anonymous'
         />
+        <meta name='description' content='Synteka Bio Test Page for interview assignment' />
+        <meta
+          name='keywords'
+          content='Synteka, bio, sample, interview, assignment, Next.js, React'
+        />
+        <meta name='author' content='Synteka Bio' />
+
+        {/* Open Graph */}
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https:/syntekabio-test.com/' />
+        <meta property='og:title' content='Synteka Bio - Test Page' />
+        <meta property='og:description' content='Synteka Bio Test Page for interview assignment' />
+        <meta property='og:image' content='https://syntekabio-test.com/og-image.jpg' />
+
+        {/* Twitter Card */}
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:url' content='https://syntekabio-test.com/' />
+        <meta property='twitter:title' content='Synteka Bio - Test Page' />
+        <meta
+          property='twitter:description'
+          content='Synteka Bio Test Page for interview assignment'
+        />
+        <meta property='twitter:image' content='https://syntekabio-test.com/twitter-image.jpg' />
+        <meta name='theme-color' content='#FFFFFF' />
       </Head>
       <body>
         <Main />
